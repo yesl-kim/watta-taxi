f5e280869c490c90384ca6042834f4c1ba86475d
Daseul <dls2tmfs2@gmail.com>
2021-05-24 21:54:28 +0900

    Modify : 초기세팅 수정

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import Routes from './Routes';
+import GlobalStyles from './styles/GlobalStyles';
+import { theme } from './styles/theme';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Routes />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
